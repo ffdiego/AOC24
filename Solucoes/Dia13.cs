6d2d079b3da1e041ea19ea96bf5ef9d494a8a9e0
Diego Fernandes <diego.silva@luz-ef.com>
2024-12-13 01:55:11 -0400

    Dia 13 wip

---
+++ Solucoes/Dia13.cs
@@ -0,0 +1,74 @@
+﻿using AOC24.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AOC24.Solucoes
+{
+    internal class Maquina()
+    {
+        public const int LIMITE_POR_BOTAO = 100;
+        public const int PRECO_BOTAO_A = 3;
+        public const int PRECO_BOTAO_B = 1;
+
+        public (int x, int y) BotaoA;
+        public (int x, int y) BotaoB;
+        public (int x, int y) Premio;
+
+        private bool PassouDoPremio((int x, int y)pos)
+        {
+            return pos.x > Premio.x || pos.y > Premio.y;
+        } 
+
+        public int Soluciona()
+        {
+            Dictionary<(int a, int b), (int, int)> cache = [];
+            bool premioMultiploEmX = true;
+            bool premioMultiploEmY = true;
+            if (!premioMultiploEmX || !premioMultiploEmY)
+            {
+                return 0;
+            }
+
+            (int x, int y) resultante = (0, 0);
+            for (int a = 0; a <= LIMITE_POR_BOTAO; a++)
+            {
+                for (int b = 0; b <= LIMITE_POR_BOTAO; b++)
+                {
+                    resultante = (BotaoA.x * a + BotaoB.x * b, BotaoA.y * a + BotaoB.y * b);
+
+                    if (PassouDoPremio(resultante))
+                    {
+                        break;
+                    }
+
+                    cache.Add((a, b), resultante);
+                }
+            }
+
+
+
+            return cache.Count(c => c.Value == this.Premio);
+        }
+    }
+
+    internal class Dia13 : ISolucionador
+    {
+
+
+        public string SolucaoParte1(string input)
+        {
+            List<Maquina> maquinas = ParsersEspecificos.CriaMaquinas(input);
+            
+            return maquinas[0].Soluciona().ToString();
+        }
+
+        public string SolucaoParte2(string input)
+        {
+            return "0";
+        }
+    }
+}
---
+++ Utils/ParsersEspecificos.cs
@@ -0,0 +1,38 @@
+﻿using AOC24.Solucoes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AOC24.Utils
+{
+    internal static class ParsersEspecificos
+    {
+        public static List<Maquina> CriaMaquinas(string input)
+        {
+            const string pattern = @"Button A: X\+(?<X1>\d+), Y\+(?<Y1>\d+)\s+Button B: X\+(?<X2>\d+), Y\+(?<Y2>\d+)\s+Prize: X=(?<X3>\d+), Y=(?<Y3>\d+)";
+            Regex regex = new Regex(pattern, RegexOptions.Compiled);
+
+            List<Maquina> list = [];
+
+            foreach (string pedaco in input.Split("\n\n"))
+            {
+                Match match = regex.Match(pedaco);
+
+                if (match.Success)
+                {
+                    list.Add(new Maquina()
+                    {
+                        BotaoA = (int.Parse(match.Groups["X1"].Value), int.Parse(match.Groups["Y1"].Value)),
+                        BotaoB = (int.Parse(match.Groups["X2"].Value), int.Parse(match.Groups["Y2"].Value)),
+                        Premio = (int.Parse(match.Groups["X3"].Value), int.Parse(match.Groups["Y3"].Value)),
+                    });
+                }
+            }
+
+            return list;
+        }
+    }
+}
